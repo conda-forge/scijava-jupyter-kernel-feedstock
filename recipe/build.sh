db79dad7efb52484a87e3eb8c231f0548ec5c46a
Hadrien Mary <hadrien.mary@gmail.com>
2017-04-20 21:56:03 -0400

    Use dependency Maven plugin for installation

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,11 +7,15 @@ cd $RECIPE_DIR/
 
 git clone https://github.com/twosigma/beakerx.git
 cd beakerx/
-git checkout 585f07c5dfe7f9f0f97053d90ffc5a696d972382
+git checkout 3b7dc90e209a73516d5241b10fd72148301fde0d
 ./gradlew -p kernel/base publishToMavenLocal
 cd $RECIPE_DIR/ && rm -fr beakerx
 
 # Install Scijava Jupyter Kernel
 cd $SRC_DIR/
 mkdir "$PREFIX/opt/scijava-jupyter-kernel"
-mvn install -Pimagej --settings "$RECIPE_DIR/settings.xml"
+mvn clean install -Pconda --settings "$RECIPE_DIR/settings.xml"
+mvn dependency:copy-dependencies -Pconda --settings "$RECIPE_DIR/settings.xml"
+
+# TODO: tell Maven to also copy the main artifact !
+cp $SRC_DIR/target/scijava-jupyter-kernel-*.jar "$PREFIX/opt/scijava-jupyter-kernel/"
