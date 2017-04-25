b839fb0832678f24e5d730b1217ccb3469303433
Hadrien Mary <hadrien.mary@gmail.com>
2017-04-24 20:49:03 -0400

    Version 0.2.6 and remove manual Beakerx installation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,22 +1,5 @@
 @echo on
 
-rem Install Beakerx...waiting for a Maven artifact.
-rem This manual installation will be removed as soon as BeakerX release Maven artifact.
-
-cd "%RECIPE_DIR%"
-git clone https://github.com/twosigma/beakerx.git
-if errorlevel 1 exit 1
-
-cd beakerx\
-git checkout 3b7dc90e209a73516d5241b10fd72148301fde0d
-if errorlevel 1 exit 1
-
-call gradlew.bat -p kernel\base publishToMavenLocal
-if errorlevel 1 exit 1
-
-cd "%RECIPE_DIR%"
-RMDIR beakerx
-
 rem rem Install Scijava Jupyter Kernel
 cd "%SRC_DIR%"
 md "%PREFIX%\opt\scijava-jupyter-kernel"
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -euo
 
-# Install Beakerx...
-# ...waiting for a Maven artifact.
-# This manual installation will be removed as soon as BeakerX release Maven artifact.
-cd $RECIPE_DIR/
-
-git clone https://github.com/twosigma/beakerx.git
-cd beakerx/
-git checkout 3b7dc90e209a73516d5241b10fd72148301fde0d
-./gradlew --no-daemon -p kernel/base publishToMavenLocal
-cd $RECIPE_DIR/ && rm -fr beakerx
-
 # Install Scijava Jupyter Kernel
 cd $SRC_DIR/
 mkdir -p "$PREFIX/opt/scijava-jupyter-kernel"
