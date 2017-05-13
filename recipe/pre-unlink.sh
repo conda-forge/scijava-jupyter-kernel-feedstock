6f5fcfae6bf7d89b9ad68763a905f8307ac288b7
Hadrien Mary <hadrien.mary@gmail.com>
2017-05-13 09:45:59 -0400

    Bump to 0.3.0

--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -1,11 +1,3 @@
 #!/bin/bash -euo
 
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-python 2> /dev/null
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-groovy 2> /dev/null
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-java 2> /dev/null
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-clojure 2> /dev/null
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-r 2> /dev/null
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-scala 2> /dev/null
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-beanshell 2> /dev/null
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-ruby 2> /dev/null
-"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava-javascript 2> /dev/null
+"${PREFIX}/bin/jupyter" kernelspec remove -fy scijava 2> /dev/null
